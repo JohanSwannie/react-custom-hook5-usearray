266604e64742e281205311df1eceb86cd8e93bdf
jswan <jswannieman@gmail.com>
2023-08-28 14:04:57 +1200

    Commit useArray Javascript Component

---
+++ src/useArray.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+const useArray = (defaultValue) => {
+  const [array, setArray] = useState(defaultValue);
+
+  const push = (item) => {
+    setArray([...array, item]);
+  };
+
+  const update = (idx, item) => {
+    setArray([
+      ...array.slice(0, idx),
+      item,
+      ...array.slice(idx + 1, array.length - 1),
+    ]);
+  };
+
+  const filter = (callback) => {
+    setArray(array.filter(callback));
+  };
+
+  const remove = (idx) => {
+    setArray([
+      ...array.slice(0, idx),
+      ...array.slice(idx + 1, array.length - 1),
+    ]);
+  };
+
+  const clear = () => {
+    setArray([]);
+  };
+
+  return { array, set: setArray, push, update, filter, remove, clear };
+};
+
+export default useArray;
